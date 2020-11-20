b50c1a30593fa3366195f334d4d6480545534165
Oussama Khalfaoui <oussamakhalfaoui1@gmail.com>
2020-11-20 22:08:13 +0100

    Added some basic styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App Todo">
       <p id = "title">Add a Todo</p>
       <input type = "text"></input>
+      <div className = "box">
+        <div className = "boxheader">
+          <button>Add</button><button>Clear Selected</button><button>Clear All</button>
+          </div>
+
+
+        <div><p><input type = "checkbox"></input>First to do</p></div>
+        <div><p><input type = "checkbox"></input>Second to do </p></div>
+        
+      </div>
     </div>
   );
 }
