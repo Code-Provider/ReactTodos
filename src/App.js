f8637829ff1f8183300e64174caf99a7117e02b8
Oussama Khalfaoui <oussamakhalfaoui1@gmail.com>
2020-11-20 21:12:07 +0100

    Created React project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App Todo">
+      <p id = "title">Add a Todo</p>
+      <input type = "text"></input>
+    </div>
+  );
+}
+
+export default App;
